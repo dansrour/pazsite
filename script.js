68e444e1bc988ff04334ab1a1b04c038fec1d1ed
dansrour <dan24178@gmail.com>
2021-03-16 12:46:50 +0200

    gradient and form validation

---
+++ script.js
@@ -0,0 +1,6 @@
+'use strict';
+function submitForm() {
+  document.getElementById('myform').className += ' form-submitted';
+  //document.getElementById('myform').classList.add('form-submitted');
+  // var email = document.getElementById('email').value;
+}
